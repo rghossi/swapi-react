779afa5cf2a64b7c6ac3b655be755493b0cef0a7
rghossi <rafael.ghossi@gmail.com>
2017-08-30 00:24:31 -0300

    Add flip animation to film items

--- client/src/films/FilmItem.jsx
+++ client/src/films/FilmItem.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import './FilmItem.css';
 
-const FilmItem = ({ id, title, episodeId, openingCrawl, onClick }) => (
-  <div className="film-item" onClick={onClick}>
-    <h3>{title}</h3>
-    <h4>Episode {episodeId}</h4>
-    {openingCrawl}
-  </div>
-)
+class FilmItem extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      editMode: false
+    }
+  }
+
+  render() {
+    const { id, title, episodeId, openingCrawl, onClick } = this.props
+    const wrapper = classNames({"flip-container": true, "film-item-wrapper": true, "flip": this.state.editMode})
+    return (
+      <div className={wrapper}>
+        <div className="flipper">
+          <div className="front film-item-show-mode" onClick={() => this.setState({editMode: true})}>
+            <h3>{title}</h3>
+            <h4>Episode {episodeId}</h4>
+            {openingCrawl}
+          </div>
+          <div className="back film-item-edit-mode">
+            XABBLAU
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 FilmItem.propTypes = {
   onClick: PropTypes.func.isRequired,
