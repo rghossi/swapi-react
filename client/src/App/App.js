1f27c7c07daf0041a29a156d1b74cf108ba1e142
rghossi <rafael.ghossi@gmail.com>
2017-08-30 04:19:20 -0300

    Add new logo and favicon

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import FilmContainer from '../films/FilmContainer';
-import logo from './logo.svg';
+import logo from './sw-logo.png';
 import './App.css';
 
 class App extends Component {
